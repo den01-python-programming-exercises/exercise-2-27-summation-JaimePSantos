a53dd8aeae82fb8b03d57d37cfc67bdc3b89cd0e
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 19:40:54 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
+def sum(number1, number2, number3, number4):
+    # write your code here
+    # remember to include return (at the end)!
+    soma = number1 + number2 +number3 + number4
+    return soma
+
 def main():
-    #write your code below this line
+    answer = sum(4, 3, 6, 1)
+    print("Sum: " + str(answer))
 
 if __name__ == '__main__':
     main()
